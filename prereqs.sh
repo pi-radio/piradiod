bac68d1b268b210533ac77956404513034256bec
Michael Zappe <zapman@pi-rad.io>
2024-06-20 22:01:48 +0000

    Added prerequisites

--- prereqs.sh
+++ prereqs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo apt install -y libsdbus-c++-dev 
+git submodule update --init --recursive
+
+sudo apt install -y libsdbus-c++-dev python3-click-man protobuf-compiler protobuf-compiler-grpc libgrpc-dev libgrpc++-dev grpc-proto python3-grpc-tools python3-grpcio libfmt-dev libi2c-dev
